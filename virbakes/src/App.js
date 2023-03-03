3e49c05f094b41b8731dae5bef5699f521b9d04c
Virginia Villalobos <virginiavt98@gmail.com>
2023-03-03 23:59:54 +0100

    another path try

--- virbakes/src/App.js
+++ virbakes/src/App.js
@@ -19,11 +19,11 @@ function App() {
   return (
     
     <>
-    <Router >
+    <Router basename="/horneadoconcarino">
       <Navbar/>
      
       <Routes>
-      <Route  path='/'  element={<Home/>} />
+      <Route  exact path='/'  element={<Home/>} />
       <Route path='/products' element={<Products/>}/>
       <Route path='/custom' element={<Custom/>}/>
       <Route path='/contact'element={<Contact/>}/>
